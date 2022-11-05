97d58cbc07a8fe8e76472c288a76400a1c380e7f
Giancarlo De Roberto <15658199+gVirtu@users.noreply.github.com>
2022-11-05 02:12:06 -0300

    Add nicer styles

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ import { SaveParser } from "./saveParser.js";
 
 // DOM Elements //
 
+const DROPZONE_SELECTOR = document.getElementById("drop-zone");
 const FILE_SELECTOR = document.getElementById("save-file-input");
-const OUTPUT_SELECTOR = document.getElementById("save-contents");
+const LABEL_SELECTOR = document.getElementById("save-file-input-label");
+const OUTPUT_SELECTOR = document.getElementById("output");
+const ERROR_SELECTOR = document.getElementById("error-detail");
+const ERROR_CONTENT_SELECTOR = document.getElementById("error-content");
+const OUTPUT_TEXT_SELECTOR = document.getElementById("formatted-team");
 const BUTTON_SELECTOR = document.getElementById("copy-button");
 
 //////////////////
 
+// File input and save parsing
 function readSave(file) {
+  LABEL_SELECTOR.innerText = file.name;
+  OUTPUT_SELECTOR.classList.remove("show");
+  ERROR_SELECTOR.classList.remove("show");
+
   const reader = new FileReader();
   const parser = new SaveParser();
   let formatter;
@@ -17,18 +27,19 @@ function readSave(file) {
   reader.addEventListener("load", (event) => {
     const bytes = new Uint8Array(event.target.result);
 
-    const save = parser.parseSave(bytes);
-    formatter = new TeamFormatter(save);
+    try {
+      const save = parser.parseSave(bytes);
+      formatter = new TeamFormatter(save);
 
-    OUTPUT_SELECTOR.value = formatter.format();
+      OUTPUT_SELECTOR.classList.add("show");
+      OUTPUT_TEXT_SELECTOR.value = formatter.format();
+    } catch (error) {
+      ERROR_SELECTOR.classList.add("show");
+      ERROR_CONTENT_SELECTOR.innerText = error.stack;
+    }
   });
 
   reader.readAsArrayBuffer(file);
-}
-
-function copyFormattedOutput() {
-  OUTPUT_SELECTOR.select();
-  navigator.clipboard.writeText(OUTPUT_SELECTOR.value);
 }
 
 FILE_SELECTOR.addEventListener("change", (event) => {
@@ -38,5 +49,59 @@ FILE_SELECTOR.addEventListener("change", (event) => {
     readSave(fileList[0]);
   }
 });
+
+LABEL_SELECTOR.addEventListener("click", (event) => {
+  FILE_SELECTOR.click();
+});
+
+// Dropzone shenanigans
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+function highlight() {
+  DROPZONE_SELECTOR.classList.add("highlight");
+}
+
+function unhighlight() {
+  DROPZONE_SELECTOR.classList.remove("highlight");
+}
+
+function handleDrop(e) {
+  const dt = e.dataTransfer;
+  const fileList = dt.files;
+
+  if (fileList.length > 0) {
+    readSave(fileList[0]);
+  }
+}
+
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  DROPZONE_SELECTOR.addEventListener(eventName, preventDefaults, false);
+});
+
+[("dragenter", "dragover")].forEach((eventName) => {
+  DROPZONE_SELECTOR.addEventListener(eventName, highlight, false);
+});
+
+["dragleave", "drop"].forEach((eventName) => {
+  DROPZONE_SELECTOR.addEventListener(eventName, unhighlight, false);
+});
+
+DROPZONE_SELECTOR.addEventListener('drop', handleDrop, false);
+
+// Copy button
+function resetCopyButtonText() {
+  BUTTON_SELECTOR.innerText = '📋 Copy to clipboard';
+}
+
+function copyFormattedOutput() {
+  OUTPUT_TEXT_SELECTOR.select();
+  navigator.clipboard.writeText(OUTPUT_TEXT_SELECTOR.value);
+  BUTTON_SELECTOR.innerText = '✔️ Copied!';
+
+  setTimeout(resetCopyButtonText, 3000);
+}
 
 BUTTON_SELECTOR.addEventListener("click", copyFormattedOutput);
